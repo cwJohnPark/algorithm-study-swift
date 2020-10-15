4c5acfbe0e3756ffcb46d653f5d99ef5c39daffb
cwJohnPark <pierrotcw@gmail.com>
2020-10-16 05:49:17 +0900

    [Trees] List Of Depth

--- Trees/ListOfDepths.playground/Contents.swift
+++ Trees/ListOfDepths.playground/Contents.swift
@@ -6,15 +6,16 @@
  */
 
 /*
+ * Depth-first search
  * pre-order traversal
  */
 func createLevelLinkedList(_ root: TreeNode<Int>) -> Array<LinkedList<TreeNode<Int>>> {
     var lists = Array<LinkedList<TreeNode<Int>>>()
-    createLevelLinkedList(root, &lists, 0)
+    createLevelLinkedList_DFS(root, &lists, 0)
     return lists
 }
 
-func createLevelLinkedList(_ root: TreeNode<Int>?, _ lists: inout Array<LinkedList<TreeNode<Int>>>, _ level: Int) {
+func createLevelLinkedList_DFS(_ root: TreeNode<Int>?, _ lists: inout Array<LinkedList<TreeNode<Int>>>, _ level: Int) {
     guard let rootNode = root else {
         return;
     }
@@ -28,14 +29,105 @@ func createLevelLinkedList(_ root: TreeNode<Int>?, _ lists: inout Array<LinkedLi
         list = lists[level]
     }
     list.add(rootNode)
-    createLevelLinkedList(rootNode.left, &lists, level + 1)
-    createLevelLinkedList(rootNode.right, &lists, level + 1)
+    createLevelLinkedList_DFS(rootNode.left, &lists, level + 1)
+    createLevelLinkedList_DFS(rootNode.right, &lists, level + 1)
     
+}
+
+/*
+ * Breadth-first search
+ */
+func createLevelLinkedList_BFS(_ root: TreeNode<Int>) -> Array<LinkedList<TreeNode<Int>>> {
+    var result = Array<LinkedList<TreeNode<Int>>>()
+    
+    var current = LinkedList<TreeNode<Int>>()
+    // add root
+    current.add(root)
+    
+    while current.count > 0 {
+        
+        result.append(current)
+        
+        let parents = current
+        current = LinkedList<TreeNode<Int>>()
+        for index in 0 ..< parents.count {
+            
+            let parent = parents.get(at: index)
+            
+            if let left = parent?.data.left {
+                current.add(left)
+            }
+            if let right = parent?.data.right {
+                current.add(right)
+            }
+            
+        }
+    }
+    
+    return result
+}
+
+/*
+ * Breadth-first Search Using Array
+ */
+func createLevelArrayList_BFS(_ root: TreeNode<Int>) -> Array<Array<TreeNode<Int>>> {
+    var result = Array<Array<TreeNode<Int>>>()
+    
+    var current = Array<TreeNode<Int>>()
+    // add root
+    current.append(root)
+    
+    while current.count > 0 {
+        
+        result.append(current)
+        
+        let parents = current
+        current = Array<TreeNode<Int>>()
+        
+        for parent in parents {
+                        
+            if let left = parent.left {
+                current.append(left)
+            }
+            if let right = parent.right {
+                current.append(right)
+            }
+            
+        }
+    }
+    
+    return result
 }
 
 
 class LinkedList<T> {
     var head: LinkedListNode<T>?
+    
+    var count: Int {
+        guard var root = head else {
+            return 0
+        }
+        var count = 1
+        
+        while let n = root.next {
+            root = n
+            count += 1
+        }
+        return count
+    }
+    
+    func get(at index: Int) -> LinkedListNode<T>? {
+        var count = 0
+        var currentNode = head
+        
+        while count < index && currentNode != nil {
+            currentNode = currentNode?.next
+            count += 1
+        }
+        
+        return currentNode
+        
+    }
     
     func add(_ node: T) {
         guard var nextNode = head else {
@@ -48,6 +140,7 @@ class LinkedList<T> {
         }
         nextNode.next = LinkedListNode(node)
     }
+    
 }
 
 class LinkedListNode<T> {
@@ -72,7 +165,7 @@ class TreeNode<T> {
 
 
 /*
- * ex1
+ * ex1 - DFS
  * input: 1 - 2, 5 - 3, 4, 6, 7
  * output: [1,[2,5],[3,4,6,7]]
  */
@@ -93,3 +186,21 @@ for linkedList in result_ex1 {
 }
 
 
+/*
+ * ex2 - BFS
+ * input: 1 - 2, 5 - 3, 4, 6, 7
+ * output: [1,[2,5],[3,4,6,7]]
+ */
+var ex2_tree = TreeNode(1,
+                        TreeNode(2,
+                                 TreeNode(3), TreeNode(4)),
+                        TreeNode(5,
+                                 TreeNode(6), TreeNode(7)))
+
+var result_ex2 = createLevelArrayList_BFS(ex2_tree)
+for list in result_ex2 {
+    for node in list {
+        print(node.data, terminator: " ")
+    }
+    print()
+}
