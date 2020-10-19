8d381cd8202f0093bbfef907ff7806b330487d73
cwJohnPark <pierrotcw@gmail.com>
2020-10-20 05:51:34 +0900

    [Trees] Check BST using min,max

--- Trees/CheckBST.playground/Contents.swift
+++ Trees/CheckBST.playground/Contents.swift
@@ -29,6 +29,35 @@ func checkBST(_ root: TreeNode<Int>?) -> Bool {
     
 }
 
+/*
+ * Min-max solution
+ */
+func checkBST_2(_ n: TreeNode<Int>?) -> Bool {
+    return checkBST_2_helper(n, nil, nil)
+}
+func checkBST_2_helper(_ n: TreeNode<Int>?, _ min: Int?, _ max: Int?) -> Bool {
+    guard let rootNode = n else {
+        return true
+    }
+    
+    if let minValue = min {
+        if rootNode.data <= minValue {
+            return false
+        }
+    }
+    if let maxValue = max {
+        if rootNode.data > maxValue {
+            return false
+        }
+    }
+    
+    if checkBST_2_helper(rootNode.left, min, rootNode.data) == false ||
+        checkBST_2_helper(rootNode.right, rootNode.data, max) == false {
+        return false
+    }
+    return true
+}
+
 class TreeNode<T> {
     var data: T
     var left: TreeNode?
@@ -54,3 +83,5 @@ let ex1 = TreeNode(4,
 let ex1Result = checkBST(ex1)
 print(ex1Result)
 
+let ex1Result2 = checkBST_2(ex1)
+print(ex1Result2)
